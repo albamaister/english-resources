d8b27d87f567a9b39412f5157a63166e08c1e6ab
Bryan Alba <bryanalba92@gmail.com>
2025-08-06 16:50:35 -0500

    Add Jest testing framework and create tests for Header component
    - Updated package.json to include Jest and related testing libraries.
    - Added tests for the Header component to verify rendering and search functionality.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './', // 👈 root of your app
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
