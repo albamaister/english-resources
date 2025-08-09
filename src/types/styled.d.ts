263521af495b8a4a6d1945371624c7fc4663fe80
Bryan Alba <bryanalba92@gmail.com>
2025-08-09 13:03:37 -0500

    update config

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,3 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import 'styled-components';
+import type { AppTheme } from '../lib/theme';
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends AppTheme {}
+}
+
 import 'styled-components';
 import type { Theme } from '../lib/theme';
 
